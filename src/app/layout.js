75454727d19244263405604b483ba18748f25589
Rajesh <rajesh11022002@gmail.com>
2023-09-15 10:11:39 +0530

    folder Structure done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
-import Navbar from '../../components/navbar';
-import Footer from '../../components/footer';
+import Navbar from '../../components/Navbar/navbar';
 const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({ children }) {
@@ -9,8 +8,8 @@ export default function RootLayout({ children }) {
         <html lang="en">
             <body className={inter.className}>
                 <Navbar />
+
                 {children}
-                <Footer />
             </body>
         </html>
     );
