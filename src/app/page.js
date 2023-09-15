c04de23921cc80808949ce3e1221ca8730e30f7a
abhi199kr <abhishek@appsdeployer.com>
2023-09-15 14:51:02 +0530

    abhis

--- src/app/page.js
+++ src/app/page.js
@@ -158,6 +158,17 @@ useEffect(() => {
 
             </section>
 
+            <Pagination
+      total={10}
+      classNames={{
+        wrapper: "gap-0 overflow-visible h-8 rounded border border-divider my-10",
+        item: "w-8 h-8 text-small rounded-none bg-transparent",
+        cursor:
+          "bg-gradient-to-b shadow-lg from-default-500 to-default-800 dark:from-default-300 dark:to-default-100 text-white font-bold",
+      }}
+      onChange={pagginationHandler}
+    />
+
         </>
     );
 }
